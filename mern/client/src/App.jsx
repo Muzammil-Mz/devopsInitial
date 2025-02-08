665e2f0986a68b60e26dffe8c92d16ab60a25329
Muzammil-Mz <Mujjuimran74@gmail.com>
2025-02-08 06:21:16 +0000

    steps cleared

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <h2 className='text-8xl'>Hello World from frontend</h2>
+  )
+}
+
+export default App
---
+++ mern/server/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
+
+
+
+const app=express()
+const port=5002
+
+app.use(express.static(path.join (__dirname,"dist")))
+app.get("/suhail",(req,res)=>{
+    res.status(200).json({message:"HELLO Suhail"})
+})
+
+app.get("/*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"dist","index.html"))
+})
+
+app.listen(port,()=>{
+    console.log(`Server is up and listeing `);
+})
